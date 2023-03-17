9d07adfcd52f32bcf4f6742aab57adf9ab474b69
karishmabisht14 <karishma.qt14@gmail.com>
2023-03-18 01:47:39 +0530

    Create javascript variables in components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,17 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 2, 17);
+  const expenseTitle = "Food";
+  const expenseAmount = "100";
   return (
     <div>
-      <h2>Expense Items</h2>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movies Rs 200</p>
+      <h2 className="expense-item">Expense Items</h2>
+      <div>{expenseDate.toISOString()}</div>
+      <div>
+        <h2>{expenseTitle}</h2>
+        <div>Rs{expenseAmount}</div>
+      </div>
     </div>
   );
 }
