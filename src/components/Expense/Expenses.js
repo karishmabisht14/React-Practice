d90d929ea54a935b281f7da1310d82e13ddc77bd
karishmabisht14 <karishma.qt14@gmail.com>
2023-03-27 13:38:48 +0530

    Create Chart  App

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -23,6 +24,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
